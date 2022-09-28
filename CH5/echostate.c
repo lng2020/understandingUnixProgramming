84fb8551f1d034dbf6169c7cc7a2f66c70f88b87
lng2020 <nanguanlin6@gmail.com>
2022-09-28 05:56:46 -0400

    code for CH6

---
+++ CH5/echostate.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+
+int main()
+{
+    struct termios info;
+    int rv;
+
+    rv = tcgetattr( 0, &info);
+    if ( rv == -1){
+        perror("tcgetattr");
+        exit(1);
+    }
+
+    if (info.c_lflag & ECHO){
+        printf("echo is on, since its bit is 1\n");
+    }
+    else{
+        printf("ehco is OFF, since its bit is 0\n");
+    }
+    return 0;
+}
---
+++ CH6/atm.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+while true
+do 
+    do_a_transaction
+    if play_again
+    then 
+        continue
+    fi
+    break
+done
+
